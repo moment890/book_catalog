26933d2d993b1b297a3ebb46af4e375ae4fd6da0
User <User@WIN-H77MA6T5FBT>
2026-04-25 15:00:22 +0500

    начел рабоду с моделью

---
+++ book_catalog/Models/Genre_Books.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace book_catalog.Models {
+    public class Genre_Books 
+    {
+        public int Genre_Id { get; set; }
+        public string Genre_Name { get; set; }
+        public int Category_Id { get; set; }
+    }
+}
---
+++ book_catalog/Models/Title_Books.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace book_catalog.Models {
+    public class Title_Books 
+    {
+        public int Book_Id { get; set; }
+        public string Book_Title { get; set; }
+        public List<Genre_Id> Gebre_Books { get; set; }
+    }
+}

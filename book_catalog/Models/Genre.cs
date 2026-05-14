d84deec415d9d51037465be847c5ae3a0953443f
User <User@WIN-H77MA6T5FBT>
2026-05-14 15:18:02 +0500

    убрал все ошибки но чу-чуть запуталса с  визуальной частью , и с миграцией.

--- book_catalog/Models/Genre.cs
+++ book_catalog/Models/Genre.cs
@@ -10,6 +10,7 @@ namespace book_catalog.Models {
         public int Id { get; set; }
         public string Name { get; set; }
 
+
         // Навигационное свойство для связи с книгами
         public ICollection<Book> Books { get; set; } = new List<Book>();
     }
